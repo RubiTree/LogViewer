caae818ac61e67d60533a30fb8217c32dda08616
bigbear <xiongzhengyu@hqyxjy.com>
2018-06-10 02:45:16 +0800

    pre push to github

--- main.js
+++ main.js
@@ -63,3 +63,16 @@ history.pushState(null, null, document.URL);
 window.addEventListener('popstate', function () {
     history.pushState(null, null, document.URL);
 });
+
+console.log(getQueryVariable('redirectUrl'));
+
+// 获取参数
+function getQueryVariable(variable){
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
