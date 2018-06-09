ceb94d847534a932d4bc70b76bcee7e549a43f7c
bigbear <xiongzhengyu@hqyxjy.com>
2018-06-10 03:07:17 +0800

    test ajax

--- main.js
+++ main.js
@@ -13,30 +13,30 @@ function dropHandler(e) {
     var file = files[0];
     var reader = new FileReader();
 
-    reader.onload = function(e){
+    reader.onload = function(e) {
         var logText = this.result;
         var logLines = logText.split('\n');
         var logLineSize = logLines.length;
 
         var foundIndex = [];
         var regex = /进入界面/;
-        for(var i = 0; i < logLines.length; i++){
-            if(logLines[i].search(regex) > -1){
+        for (var i = 0; i < logLines.length; i++) {
+            if (logLines[i].search(regex) > -1) {
                 foundIndex.push(i);
             }
         }
 
         var canvas = document.getElementById('canvas');
-        var ctx=canvas.getContext("2d");
-        for(var i = 0;i<foundIndex.length;i++){
-            var positionY = (foundIndex[i]/logLineSize)*900;
-            ctx.moveTo(0,positionY);
-            ctx.lineTo(200,positionY);
+        var ctx = canvas.getContext("2d");
+        for (var i = 0; i < foundIndex.length; i++) {
+            var positionY = (foundIndex[i] / logLineSize) * 900;
+            ctx.moveTo(0, positionY);
+            ctx.lineTo(200, positionY);
         }
         ctx.stroke();
 
-        for(var i = 0; i < logLines.length; i++){
-            logLines[i] = '<p>' + logLines[i].substr(1,100) + '</p>';
+        for (var i = 0; i < logLines.length; i++) {
+            logLines[i] = '<p>' + logLines[i].substr(1, 100) + '</p>';
         }
         p.innerHTML = logLines.join('');
     }
@@ -45,11 +45,16 @@ function dropHandler(e) {
     reader.readAsText(file);
 }
 
-function scrollArea(e){
+function scrollArea(e) {
     var p = document.getElementById('input_log_area');
     var contentHeight = p.scrollHeight;
-    p.scrollTop = (e.clientY/900)*contentHeight - 400;
+    p.scrollTop = (e.clientY / 900) * contentHeight - 400;
 }
+
+// ---
+get(getQueryVariable('redirectUrl'), function(result){
+    console.log(result);
+});
 
 var drag = document.getElementById('input_log_area');
 drag.addEventListener('drop', dropHandler, false);
@@ -58,21 +63,45 @@ drag.addEventListener('dragover', dragOverHandler, false);
 var canvas = document.getElementById('canvas');
 canvas.addEventListener('click', scrollArea, false);
 
+
+// ----
+
 // 禁用页面回退
 history.pushState(null, null, document.URL);
-window.addEventListener('popstate', function () {
+window.addEventListener('popstate', function() {
     history.pushState(null, null, document.URL);
 });
 
-console.log(getQueryVariable('redirectUrl'));
-
 // 获取参数
-function getQueryVariable(variable){
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
+    return (false);
+}
+
+
+function get(url,callback){
+  var xhr = new XMLHttpRequest();  //创建新请求
+  xhr.open('GET',url);
+  xhr.onreadystatechange=function(){
+    //如果请求完成且成功
+    if(xhr.readyState === 4 && xhr.status === 200){
+      //获得响应的类型
+      var type = xhr.getResponseHeader('Content-type');
+      if(type.indexOf('xml') !== -1 && xhr.responseXML){
+        callback(xhr.responseXML);  //Document对象响应
+      }else if(type === 'application/json'){
+        callback(JSON.parse(xhr.responseText));  //JSON响应
+      }else{
+        callback(xhr.responseText);  //字符串响应
+      }
+    }
+  };
+  xhr.send(null); //立即发送请求
 }
