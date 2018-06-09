79c959f5626ca8a7036f136de06f3e5f40491268
bigbear <xiongzhengyu@hqyxjy.com>
2018-06-09 18:38:53 +0800

    实现canvas绘制目标内容位置和点击定位

--- main.js
+++ main.js
@@ -14,17 +14,52 @@ function dropHandler(e) {
     var reader = new FileReader();
 
     reader.onload = function(e){
-        var div = document.createElement('div');
-        div.className = "text"
-        div.innerHTML = this.result;
+        var logText = this.result;
+        var logLines = logText.split('\n');
+        var logLineSize = logLines.length;
 
-        p.insertBefore(div, null);
+        var foundIndex = [];
+        var regex = /进入界面/;
+        for(var i = 0; i < logLines.length; i++){
+            if(logLines[i].search(regex) > -1){
+                foundIndex.push(i);
+            }
+        }
+
+        var canvas = document.getElementById('canvas');
+        var ctx=canvas.getContext("2d");
+        for(var i = 0;i<foundIndex.length;i++){
+            var positionY = (foundIndex[i]/logLineSize)*900;
+            ctx.moveTo(0,positionY);
+            ctx.lineTo(200,positionY);
+        }
+        ctx.stroke();
+
+        for(var i = 0; i < logLines.length; i++){
+            logLines[i] = '<p>' + logLines[i].substr(1,100) + '</p>';
+        }
+        p.innerHTML = logLines.join('');
     }
 
     //读取文件内容
     reader.readAsText(file);
 }
 
+function scrollArea(e){
+    var p = document.getElementById('input_log_area');
+    var contentHeight = p.scrollHeight;
+    p.scrollTop = (e.clientY/900)*contentHeight - 400;
+}
+
 var drag = document.getElementById('input_log_area');
 drag.addEventListener('drop', dropHandler, false);
 drag.addEventListener('dragover', dragOverHandler, false);
+
+var canvas = document.getElementById('canvas');
+canvas.addEventListener('click', scrollArea, false);
+
+// 禁用页面回退
+history.pushState(null, null, document.URL);
+window.addEventListener('popstate', function () {
+    history.pushState(null, null, document.URL);
+});
